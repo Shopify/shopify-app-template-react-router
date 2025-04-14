2c43f53dbaebda0bad3e32b29b34a75944183b38
Richard Powell <richard.powell@shopify.com>
2025-04-14 15:00:48 -0400

    React Router: Added react-router.config.ts as per https://reactrouter.com/upgrading/remix\#5-add-a-react-router-config

--- vite.config.ts
+++ vite.config.ts
@@ -55,17 +55,7 @@ export default defineConfig({
     },
   },
   plugins: [
-    reactRouter({
-      ignoredRouteFiles: ["**/.*"],
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-        v3_singleFetch: true,
-        v3_routeConfig: true,
-      },
-    }),
+    reactRouter(),
     tsconfigPaths(),
   ],
   build: {
