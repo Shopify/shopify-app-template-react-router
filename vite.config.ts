53df6e76cc80d88e9c8930a5d791fa8ccc2594d2
Richard Powell <richard.powell@shopify.com>
2025-04-14 14:49:42 -0400

    React Router: Ran npx codemod remix/2/react-router/upgrade && npm install as per https://reactrouter.com/upgrading/remix#2-update-dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { vitePlugin as remix } from "@remix-run/dev";
+import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -55,7 +55,7 @@ export default defineConfig({
     },
   },
   plugins: [
-    remix({
+    reactRouter({
       ignoredRouteFiles: ["**/.*"],
       future: {
         v3_fetcherPersist: true,
